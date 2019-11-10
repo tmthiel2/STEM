7925bf0da1aa668a5824e79401f4d6d91d5ccce0
tmthiel2 <57120821+tmthiel2@users.noreply.github.com>
2019-11-10 11:39:40 -0600

    Update testing.R
    Changed file to be comments for starting with for the first lecture.

--- testing.R
+++ testing.R
@@ -1,11 +1,13 @@
-# Step 1
-.
-# Step 2
-.
-# Step 3
-.
-# Step 4
-.
-.
-.
-.
+## Step 1: Practice installation of a package ##
+
+## Step 2: Reading your dataset into R ##
+
+## Step 3: Calculating the number of rows in your dataset ##
+
+## Step 4: How many observations are in your dataset? ##
+
+## Step 5: Calculating the number of columns in your dataset ##
+
+## Step 6: How many variables are in your dataset? ##
+
+## Step 7: Splitting your dataset into 60% training and 40% validation ##
