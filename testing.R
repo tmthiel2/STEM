bf471ed5703f0e7becb2733b40d3409ebfca9ea2
tmthiel2 <57120821+tmthiel2@users.noreply.github.com>
2019-11-16 21:03:57 -0600

    Updating Code file to include all comments
    This way students dont need to keep uploading new files

--- testing.R
+++ testing.R
@@ -11,3 +11,95 @@
 ## Step 6: How many variables are in your dataset? ##
 
 ## Step 7: Splitting your dataset into 60% training and 40% validation ##
+
+## Step 8: Run out the deciles to see the distributions of each numeric variable ##
+
+## Step 9: Do all the deciles look reasonable? ##
+
+## Step 10: Calculating the frequencies on categorical variables ##
+
+## Step 11: Do all of the variables appear to have sufficient number of observations in each level?  ##
+
+## Step 12: Plotting the numeric variables ##
+
+## Step 13: Do the plots reveal any interesting patterns?               ##
+
+## Step 14: Plotting the categorical variables ##
+
+## Step 15: Calculate correlations ##
+
+## Step 16: Are there variables in the dataset that we should watch out for?      ##
+
+## Step 17: Fitting a model using backward selection ##
+
+## Step 18: How many variables are chosen in the final step of backward selection? ##
+
+## Step 19: Fitting a model using forward selection ##
+
+## Step 20: How many variables are chosen in the final step of forward selection? ##
+
+## Step 21: Fitting a model using stepwise selection ##
+
+## Step 22: How many variables are chosen in the final step of stepwise selection? ##
+
+## Step 23: Fitting the final regression model ##
+
+## Step 24: Validating the Model ##
+
+## Step 25: Calculating MAPE from your predictive model ##
+
+## Step 26: What is the MAPE value your model produced? Do you think it's "good"? ##
+
+## Step 27: Calculating MAPE for random guess predictions ##
+
+## Step 28: What is the MAPE value produced by using predictions that are random guesses?  ##
+                                                                       
+## Step 29: Calculating MAPE for mean predictions ##
+
+## Step 30: What is the MAPE value produced by using predictions that are the mean of the target value?  ##
+
+## Step 31: Calculating MAPE for median predictions ##
+
+## Step 32: What is the MAPE value produced by using predictions that are the median of the target value?  ##
+
+## Step 33: Can you fill out the table with the MAPE values produced by these different prediction methods. Which method produced the lowest MAPE value and therefore the most accurate predictions?  ##
+
+## Answer: 
+
+## |Method            |MAPE    |
+## |:----------------:|:------:|
+## |Predictive Model  |%       |
+## |Median            |%       |
+## |Mean              |%       |
+## |Random Guess      |%       |
+
+## Step 34: Calculating R-Squared for predictions from your predictive model ##
+
+## Step 35: What is the R-Squared value on your validation data? Does the validation R-Squared value make sense with the MAPE value calculated above? ##
+
+## Step 36: Calculating R-Squared for random guess predictions ##
+
+## Step 37: What is the R-squared value produced by using predictions that are random guesses? ##
+
+## Step 38: Calculating R-Squared for mean predictions ##
+
+## Step 39: What is the R-squared value produced by using predictions that a mean of the target value? ##
+
+## Step 40: Calculating R-Squared for median predictions ##
+
+## Step 41: What is the R-squared value produced by using predictions that a median of the target value? ##
+
+## Step 42: Can you fill out the table with the R-square values produced by these different prediction methods. Which method produced the highest R-square value? ##
+
+## |Method            |R-square |
+## |:----------------:|:-------:|
+## |Predictive Model  |         |
+## |Median            |         |
+## |Mean              |         |
+## |Random Guess      |         |
+
+## Step 43: Re-run out model to see output ##
+
+## Step 44: If we look at the p-values in your dataset (Pr(>|t|)), how many variables have values less than 0.05? Are there any variables that even though stepwise selection chose that variable, it doesn't have a p-value less than 0.05?  ##
+
+## Step 45: How do we interpret the coefficients in your dataset?  ##
